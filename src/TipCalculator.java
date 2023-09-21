a3c56d883cf0c1aaae6c33c2544e1308155f87dc
BT_1N3_31 <sample@bths.edu>
2023-09-21 08:57:32 -0400

    3rd commit

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -6,6 +6,8 @@ public class TipCalculator {
         System.out.println("Welcome to the tip calculator! ");
         System.out.println("How many people are in your group: ");
         int numOfPeople= scan.nextInt();
+        System.out.println("What's the tip percentage? (0-100):");
+        double tipPercentage = scan.nextDouble();
         double cost;
         double totalCost = 0;
         System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
@@ -13,12 +15,21 @@ public class TipCalculator {
         while (number != -1.0) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             cost = scan.nextInt();
-            totalCost += cost
+            totalCost += cost;
 
         }
-
-        // code below the while loop runs after the loop ends
-        System.out.print("Done!");
-
+        double tip = (double)totalCost * tipPercentage/100;
+        double billAndTip = totalCost + tip;
+        double perPersonCostBeforeTip = (double)totalCost / numOfPeople;
+        double tipPerPerson = (double)tip/ numOfPeople;
+        double costPerPerson = (double)totalCost / numOfPeople;
+        System.out.println("-------------------------------- ");
+        System.out.println("Total bill before tip: " + totalCost);
+        System.out.println("Total percentage: " + tipPercentage);
+        System.out.println("Total tip: " + tip);
+        System.out.println("Total bill with tip: " + billAndTip);
+        System.out.println("Per person cost before tip: " + perPersonCostBeforeTip);
+        System.out.println("Tip per person: " + tipPerPerson);
+        System.out.println("Total cost per person: " + costPerPerson);
     }
 }
