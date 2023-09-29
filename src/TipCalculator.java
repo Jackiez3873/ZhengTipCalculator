b1bc1497454d8d179baaf8e8a9c2944c6d92cf8f
BT_1N3_31 <sample@bths.edu>
2023-09-29 08:59:41 -0400

    final 5

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -14,21 +14,26 @@ public class TipCalculator {
         double cost = 0;
 
         List <String> foodList = new ArrayList<>();
-
+        // while loop
         while (cost != -1.0) {
+
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 or -1.0 to end): ");
             cost = scan.nextDouble();
             if (cost == -1.0) {
                 break;
+                // stops loop before -1 is added or another item is asked for
             }
             totalCost += cost;
             scan.nextLine();
 
             System.out.print("Enter the item: ");
             String add = scan.nextLine();
-            foodList.add(add);
+            foodList.add(add); // stores items in list
 
         }
+
+        //rounding to 2 decimal places
+        // variables
         totalCost = Math.round(totalCost * 100 ) / 100.0;
         double tip = totalCost * tipPercentage/100;
         tip = Math.round(tip * 100 ) / 100.0;
@@ -38,22 +43,26 @@ public class TipCalculator {
         perPersonCostBeforeTip = Math.round(perPersonCostBeforeTip * 100 ) / 100.0;
         double tipPerPerson = tip/ numOfPeople;
         tipPerPerson = Math.round(tipPerPerson * 100 ) / 100.0;
-        double costPerPerson = totalCost / numOfPeople;
+        double costPerPerson = billAndTip / numOfPeople;
         costPerPerson = Math.round(costPerPerson * 100 ) / 100.0;
 
+        //print statements
         System.out.println("-------------------------------- ");
-        System.out.println("Total bill before tip: " + totalCost);
-        System.out.println("Total percentage: " + tipPercentage);
-        System.out.println("Total tip: " + tip);
-        System.out.println("Total bill with tip: " + billAndTip);
-        System.out.println("Per person cost before tip: " + perPersonCostBeforeTip);
-        System.out.println("Tip per person: " + tipPerPerson);
-        System.out.println("Total cost per person: " + costPerPerson);
+        System.out.println("Total bill before tip: $" + totalCost);
+        System.out.println("Total percentage: " + tipPercentage + "%");
+        System.out.println("Total tip: $" + tip);
+        System.out.println("Total bill with tip: $" + billAndTip);
+        System.out.println("Per person cost before tip: $" + perPersonCostBeforeTip);
+        System.out.println("Tip per person: $" + tipPerPerson);
+        System.out.println("Total cost per person: $" + costPerPerson);
         System.out.println("-------------------------------- ");
         System.out.println("Items ordered: ");
+        // print foods in foodList, EC feature
         for(String foods : foodList){
             System.out.println(foods);
         }
+
+        // freestyle feature
         billAndTip *= 100;
         int billAndTipInt = (int)billAndTip;
         int quarters = billAndTipInt / 25;
